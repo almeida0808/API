753ec7b395d369bcff3ce0820a4dfbd6274474de
almeida0808 <almeidaff08@gmail.com>
2024-02-25 20:02:54 -0300

    estudando: route params

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ const express = require("express"); // adiciona todas funcionalidades do express
 
 const app = express(); // inicia o express
 
+app.get('/message/:id/:user', (request, response) => {
+    const {id, user} = request.params
+
+    response.send(`
+    o id da menssagem é: ${id}
+    para o usuário: ${user}`)
+})
+
 const PORT = 3333; // definimos o ndereço da porta
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 // listen fica observando o endereço da nossa porta e assim que for iniciado coloca essa mensagem no console.log

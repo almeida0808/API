0df79244c9d6bdd84bc03767e42be2677bf07a47
almeida0808 <almeidaff08@gmail.com>
2024-03-04 21:30:06 -0300

    criando um banco de dados

---
+++ src/database/sqlite/index.js
@@ -0,0 +1,12 @@
+const sqlite3 = require("sqlite3"); // driver da versão do sql que vamos usar
+const sqlite = require("sqlite"); // driver que faz conectar com o banco de dados
+const path = require("path"); // biblioteca que corrige o problema relacionado com o local de armazenamento do nosso database
+
+async function sqliteConection() { // cria uma função addincorona
+  const database = await sqlite.open({  // caso n tenha nenhum banco de dados a aplicação cria, caso ja tenha ela abre
+    filename: path.resolve(__dirname, "..", "database.db"),  // indica o local do arquivo do banco de dados usando o path
+    driver: sqlite3.Database, // indica qual driver estamos usando
+  });
+  return database; // retorna o banco de dados
+}
+module.exports = sqliteConection // exporta a conexão com o banco de dados para o nosso server
--- src/server.js
+++ src/server.js
@@ -1,30 +1,31 @@
-require('express-async-errors')
-const AppError = require('./utils/AppError')
+require("express-async-errors"); // importa a biblioteca de erros do node
+const database = require("./database/sqlite"); // importa o banco de dados
+const AppError = require("./utils/AppError"); // importa as funções de erros/coisas inesperadas
 const express = require("express"); // adiciona todas funcionalidades do express
-
-const routes = require("./routes") // importa nosso arquivo de controle de rotas pro servidor
+const routes = require("./routes"); // importa nosso arquivo de controle de rotas pro servidor
 
 const app = express(); // inicia o express
 app.use(express.json()); // indica pra nossa aplicação que os dados que estão sendo capturados são no formato json.
 
-app.use(routes) // roda o nosso arquivo de rota a cada nova solicitação feita pelo usuário
+app.use(routes); // roda o nosso arquivo de rota a cada nova solicitação feita pelo usuário
 
-app.use((error, request, response, next) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: 'error',
-        message: error.message
-      })
-    }
-  
-    console.error(error)
-  
-    return response.status(500).json({
-      status: 'error',
-      message: 'Internal server error'
-    })
-  })
+database(); // roda o nosso banco de dados
 
+app.use((error, request, response, next) => { // toda vez que for requisitado algo, ele faz a função abaixo
+  if (error instanceof AppError) { // se o tipo de erro da aplicação for um erro do cliente , retorne a seguinte mensagem:
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  return response.status(500).json({ // caso não seja um erro do cliente emita uma mensagem dizendo q o erro é interno do server
+    status: "error",
+    message: "Internal server error",
+  });
+});
 
 const PORT = 3333; // definimos o ndereço da porta
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); /* LISTEN fica observando o endereço da nossa porta e assim que for iniciado coloca essa mensagem no console.log*/
+app.listen(PORT, () =>
+  console.log(`Server is running on port ${PORT}`)
+); /* LISTEN fica observando o endereço da nossa porta e assim que for iniciado coloca essa mensagem no console.log*/

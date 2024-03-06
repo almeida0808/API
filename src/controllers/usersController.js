daba998eac35e19c101865a6a799f94d59d85b62
almeida0808 <almeidaff08@gmail.com>
2024-03-05 22:02:52 -0300

    cadastrando usuarios e criptografando senhas

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,7 +1,6 @@
+const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
-
 const sqliteConnection = require("../database/sqlite");
-const { application } = require("express");
 
 class usersController {
   async create(request, response) {
@@ -9,14 +8,24 @@ class usersController {
 
     const database = await sqliteConnection(); // conecta nosso controle de usuários com o database
 
-    // verifica se o email existe 
-    const checkUserExists = await database.get('SELECT * FROM users WHERE email = (?)' , [email]) // ele procura dentro de toda a tabela se existe um email igual o do usuário que esta sendo criado esse (?) é substituido pelo dado(email) que foi informado entre []
+    // verifica se o email existe
+    const checkUserExists = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    ); // ele procura dentro de toda a tabela se existe um email igual o do usuário que esta sendo criado esse (?) é substituido pelo dado(email) que foi informado entre []
 
-    if(checkUserExists){
-      throw new AppError('Este email já existe.')
+    if (checkUserExists) {
+      throw new AppError("Este email já existe.");
     }
-return response.status(201).json()
 
+    const passwordCrypted = await hash(password, 8); // atravez da bibliocteca hash conseguimos cryptografar nossa senha
+
+    await database.run(
+      "INSERT INTO users (name, email , password) VALUES (?, ?, ?)",
+      [name, email, passwordCrypted] // ja passamos a senha criptografada
+    ); // roda o codigo para adicionar um usuário , envia pro nosso banco de dados as informações que foram capturadas no request body
+
+    return response.status(201).json();
   }
 }
 

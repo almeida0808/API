ab2daeb2561f21e1b8c3fba3be44bb2b55b17cb5
almeida0808 <almeidaff08@gmail.com>
2024-03-06 22:05:32 -0300

    atualização de senha com metodos de verificações

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs"); // importa uma fução(hash) da biblioteca bcrypts que faz a senha fcar criptografada
+const { hash, compare } = require("bcryptjs"); // importa uma fução(hash) da biblioteca bcrypts que faz a senha fcar criptografada
 const AppError = require("../utils/AppError"); // importa o nosso capturador de erros
 const sqliteConnection = require("../database/sqlite"); // importa a função que faz a conexão com nosso server
 
@@ -32,39 +32,63 @@ class usersController {
   }
 
   async update(request, response) {
-    const { name, email} = request.body; // pega o name e email enviado pelo user
+    const { name, email, password, old_password } = request.body; // pega o name e email enviado pelo user
     const { id } = request.params; // pega o id que foi informado como parametro
 
     const database = await sqliteConnection(); // nos conecta com o database
-    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]); // seleciona dentro da tabela o usuário coom o id que foi informado como parametro
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]); // basicamente seleciona a linha inteira com todos os dados do nosso usuário que teve o id informado como parametro, podemos usar user.nomeDoDado para acessar qualquer dado que esteja na tabela
 
-    if (!user) { // caso não tenha nenhum user informado
+    if (!user) {
+      // caso não tenha nenhum user informado
       throw new AppError("usuário não encontrado");
     }
 
-    const emailExist = await database.get("SELECT * FROM users WHERE email = (?)", [email]); // caso tente atualizar o email, ele verifica se esse email ja não foi cadastrado
+    const emailExist = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    ); // caso tente atualizar o email, ele verifica se esse email ja não foi cadastrado
 
+      
     if (emailExist && emailExist.id !== user.id) { // caso o email exista e não for do mesmo usuário que está tentando atualizar, ele emite um erro
       throw new AppError("Esse email já foi cadastrado.");
     }
 
     // atualiza o email e nome que foi informado
-    user.name = name 
-    user.email = email
+    user.name = name;
+    user.email = email;
+
+    // ALTERAÇÃO DE SENHA:
+
+    // caso o usúario tenha informado uma nova senha, veja se ele passou a antiga senha
+    if (password && !old_password) {  // se não passou envie um erro
+      throw new AppError("Digite sua senha antiga!");
+    }
+
+    // caso tenha informado uma nova senha e uma antiga senha:
+    if (password && old_password) {
+      const old_passwordCorrect = await compare(old_password, user.password); // ele verifica se a senha antiga realmente bate com a que estava no database
+
+      if (!old_passwordCorrect) { // caso não seja verdadeira essa antiga senha envie um erro
+        throw new AppError("Digite uma senha válida");
+      }
+
+      user.password = await hash(password, 8); // caso esteja correta , atualize a senha antiga para a senha que foi passada dessa vez(ja criptografando ela) 
+    }
 
     // roda o código que atualiza os dados la dentro da nossa tabela no database
-    await database.run(`
+    await database.run(
+      `
     UPDATE users SET 
+    password = ?,
     name = ?,
     email = ?,
     updated_at = ?
     WHERE id = ?`,
-      [user.name, user.email, new Date(), id]
+      [user.password, user.name, user.email, new Date(), id]
     );
 
     return response.json(); // retorna que foi criado com sucesso
   }
-
 }
 
 module.exports = usersController;

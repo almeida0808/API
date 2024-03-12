cea6191ba789f61e20e4469a79788f7daf80c279
Lucas Almeida <almeidaff08@gmail.com>
2024-03-12 11:20:59 -0300

    MIGRATIONS COM KNEX

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,12 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
-    }
-  },
+    },
+    migrations: {
+      directory: path.resolve(__dirname , "src", "database" , "knex" , "migrations" )
+    },
   useNullAsDefault: true 
+
+  },
 };
 
---
+++ src/database/knex/migrations/20240312125943_createNotes.js
@@ -0,0 +1,17 @@
+exports.up = (knex) =>
+  knex.schema.createTable("notes", (table) => {
+    // função do knex que cria uma tabela automaticamente
+    // vamos usar o "table." e na frente vamos colocar o tipo do dado , e entre parenteses o nome que essa coluna vai ter dentro do database
+    table.increment("id");
+    table.text("title");
+    table.text("description");
+
+    // ele pega o id do usuário que criou a nota, ou seja se o usuário não existir não tem como a nota existir
+    table.intenger("user_id").references("id").inTable("users");
+
+    // usa como padrão uma função do kenex para pegar a data e hora da criação ou modificação dessa nota
+    table.timestamp("created_at").default(knex.fn.now());
+    table.timestamp("updated_at").default(knex.fn.now());
+  });
+
+exports.down = (knex) => knex.schema.dropTable("notes");

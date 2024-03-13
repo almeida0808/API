a354d22154f670e22fe00b21d3bdf614051d76fa
Lucas Almeida <almeidaff08@gmail.com>
2024-03-13 11:22:45 -0300

    corrigindo problemas de escrita de código & criando migrates para links e tags

--- src/database/knex/migrations/20240312125943_createNotes.js
+++ src/database/knex/migrations/20240312125943_createNotes.js
@@ -1,13 +1,13 @@
-exports.up = (knex) =>
-  knex.schema.createTable("notes", (table) => {
+exports.up = knex =>
+  knex.schema.createTable("notes", table => {
     // função do knex que cria uma tabela automaticamente
     // vamos usar o "table." e na frente vamos colocar o tipo do dado , e entre parenteses o nome que essa coluna vai ter dentro do database
-    table.increment("id");
+    table.increments("id");
     table.text("title");
     table.text("description");
 
     // ele pega o id do usuário que criou a nota, ou seja se o usuário não existir não tem como a nota existir
-    table.intenger("user_id").references("id").inTable("users");
+    table.integer("user_id").references("id").inTable("users");
 
     // usa como padrão uma função do kenex para pegar a data e hora da criação ou modificação dessa nota
     table.timestamp("created_at").default(knex.fn.now());
